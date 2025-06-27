6db0e41ea52556cb924b4da8c960be4a6a531f0c
youssef9656 <tatayousse@gmail.com>
2025-06-27 12:56:32 +0100

    index et env

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express()
+const PORT = process.env.PORT || 5000
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+app.use(cors())
+app.use(express.json())
+app.use("/uploads", express.static("uploads"))
 
-// 🔗 Connexion à MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("✅ Connecté à MongoDB Atlas"))
-.catch((err) => console.error("❌ Erreur de connexion MongoDB :", err));
 
 // Route healthcheck obligatoire pour Railway
 app.get('/', (req, res) => {
   res.status(200).send('Server is running');
 });
 
-// Routes API
-const appRoutes = require("./routes/appRoute");
-app.use("/api", appRoutes);
+// Route centralisée
+const appRoutes = require("./routes/appRoute")
+app.use("/api", appRoutes)
 
-// Lancer serveur
 app.listen(PORT, () => {
-  console.log(`✅ Serveur lancé sur le port ${PORT}`);
-});
+  console.log(`✅ Serveur lancé sur http://localhost:${PORT}`)
+})
 
-// Initialiser l'admin
-const { createAdminUser } = require("./utils/initAdmin");
-createAdminUser();
+const { createAdminUser } = require("./utils/initAdmin")
+createAdminUser()
+
+
